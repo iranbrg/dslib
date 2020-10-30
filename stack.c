ee12f9edad2298b336b1a773d9e82f833a8debb2
Iran Braga <iran.brg@gmail.com>
2020-10-30 18:47:10 -0300

    Pop and push operations added

--- stack.c
+++ stack.c
@@ -13,10 +13,7 @@ typedef struct Node {
     // TODO: add function pointers to the operations (functions below) to simulate methods
 } Node;
 
-// Pointer to the first node
-static Node *head = NULL;
-
-// Pointer to the last node
+// Pointer to the node at top of the stack
 static Node *tail = NULL;
 
 // List's size
@@ -24,50 +21,42 @@ static size_t size;
 
 // TODO: implement peek() function (it returns the first node's data')
 // TODO: implement contains() function (it receives the data and returns
-// either true or false depending on the presence of the specified data in the queue)
+// either true or false depending on the presence of the specified data in the stack)
 
-// Add an element to the end of the queue
-static void enqueue(const int data) {
+// Add a node to the top of the stack
+static void push(const int data) {
     // Creates a new node
     Node *new_node = (Node *)malloc(sizeof(Node));
 
     new_node->data = data;
 
-    // Points it to old last node
+    // Points it to old top node
     new_node->nxt = tail;
 
     ++size;
 
-    if (head) {
-        // Makes the new node become the last node
-        tail = new_node;
-    } else {
-        // If it is the only node in the queue, it'll be the first and the last
-        head = tail = new_node;
-    }
+    // Makes the new node become the top node
+    tail = new_node;
 }
 
-// Delete a node at the beginning of the queue and returns its data
-static int dequeue() {
-    // If there's is some node in the queue its data will be returned
+// Delete the node at the top of the stack and returns its data
+static int pop() {
+    // If there's is some node in the stack its data will be returned
     // otherwise 'false' will be returned
-    if (head) {
-        Node *previous_node = NULL;
+    if (tail) {
+        Node *deleted_node = tail;
         int returned_data;
 
-        while (previous_node->nxt != NULL) {
-            // Find the node previous to the node which will be deleted
-            previous_node = previous_node->nxt;
-        }
+        tail = tail->nxt;
 
-        previous_node->nxt = NULL;
+        deleted_node->nxt = NULL;
 
         --size;
 
-        returned_data = head->data;
+        returned_data = deleted_node->data;
 
-        free(head);
-        head = previous_node;
+        free(deleted_node);
+        deleted_node = NULL;
 
         return returned_data;
     } else {
