0f1cf47939642b25f04a69f504e9092bfe45949f
Iran Braga <iran.brg@gmail.com>
2020-10-30 12:26:17 -0300

    Array like indexes feature removed

--- singly_linked_list.c
+++ singly_linked_list.c
@@ -9,9 +9,6 @@ typedef struct Node {
     // Pointer to next node
     struct Node *nxt;
 
-    // Array like index to indentify the node
-    size_t idx;
-
     // TODO: add function pointers to the operations (functions below) to simulate methods
 } Node;
 
@@ -43,14 +40,6 @@ static void insert_first(const int data) {
     // Points it to old first node
     new_node->nxt = head;
 
-    // All nodes' indexes will be shifted to right (incremented)
-    for (Node *node = head; node != NULL; node = node->nxt) {
-        ++node->idx;
-    }
-
-    // Set new node's index to 0 (it indicates new_node's gonna be the first node)
-    new_node->idx = 0;
-
     // Makes the new node become the first node
     head = new_node;
 
@@ -66,9 +55,6 @@ void append(const int data) {
 
     new_node->nxt = NULL;
 
-    // Set new node's index to biggest index (it indicates new_node's gonna be the last node)
-    new_node->idx = tail->idx + 1;
-
     // Makes the old last node become second last node
     tail->nxt = new_node;
 
@@ -82,28 +68,21 @@ void append(const int data) {
 static void insert_middle(const size_t idx, const int data) {
     // Creates a new node
     Node *new_node = (Node *)malloc(sizeof(Node));
+    Node *previous_node = head;
 
-    for (Node *node = head; node != NULL; node = node->nxt) {
-        if (node->idx == idx) {
-            new_node->data = data;
-
-            // The new node'll point to the node after the location it'll be inserted
-            new_node->nxt = node;
-
-            // Set the new node's index to the the position it'll be inserted
-            new_node->idx = idx;
-        } else if (node->idx == idx - 1) {
-            // The node behind the position that the new node will be inserted
-            // will point to the new node
-            node->nxt = new_node;
-        }
-
-        // All nodes after the position the new node was inserted
-        // will have their indexes shifted to right (incremented)
-        if (node->idx >= idx) {
-            ++node->idx;
-        }
+    for (int i = 0; i < idx - 1; i++) {
+        // Find the node previous to the position that the new node will be inserted
+        previous_node = previous_node->nxt;
     }
+
+    new_node->data = data;
+
+    // The new node'll point to the node after the location it'll be inserted
+    new_node->nxt = previous_node->nxt;
+
+    // The node behind the position that the new node
+    // will be inserted will point to the new node
+    previous_node->nxt = new_node;
 
     ++size;
 }
@@ -133,11 +112,6 @@ static Node *delete_first() {
 
     deleted_node->nxt = NULL;
 
-    // All nodes' indexes will be shifted to left (decremented)
-    for (Node *node = head; node != NULL; node = node->nxt) {
-        --node->idx;
-    }
-
     --size;
 
     return deleted_node;
@@ -147,17 +121,19 @@ static Node *delete_first() {
 static Node *delete_last() {
     // Node to be deleted
     Node *deleted_node = tail;
+    Node *previous_node = head;
 
-    for (Node *node = head; node != NULL; node = node->nxt) {
-        if (node->idx == size - 2) {
-            // The second last node'll no more point to the last node
-            // which means it'll become the new last node
-            node->nxt = NULL;
-
-            // Makes the second last node become the last node
-            tail = node;
-        }
+    while (previous_node->nxt != NULL) {
+        // Find the node previous to the node which will be deleted
+        previous_node = previous_node->nxt;
     }
+
+    // The second last node'll no more point to the last node
+    // which means it'll become the new last node
+    previous_node->nxt = NULL;
+
+    // Makes the second last node become the last node
+    tail = previous_node;
 
     --size;
 
@@ -170,29 +146,21 @@ static Node *delete_middle(const size_t idx) {
     Node *deleted_node = NULL;
 
     // Node previous to the node to be deleted
-    Node *previous_deleted_node = NULL;
+    Node *previous_node = head;
 
-    for (Node *node = head; node != NULL; node = node->nxt) {
-        if (node->idx == idx - 1) {
-            // Identities the node previous to the node to be deleted
-            previous_deleted_node = node;
-        } else if (node->idx == idx) {
-            // Identities the node to be deleted
-            deleted_node = node;
-
-            // It no longer points to the node at next position
-            deleted_node->nxt = NULL;
-        } else if (node->idx == idx + 1) {
-            // The previous node'll point to the node after the position it'll be deleted
-            previous_deleted_node->nxt = node;
-        }
-
-        // All nodes after the position the node was deleted
-        // will have their indexes shifted to left (decremented)
-        if (node->idx >= idx) {
-            --node->idx;
-        }
+    for (int i = 0; i < idx - 1; i++) {
+        // Find the node previous to the position that the new node will be inserted
+        previous_node = previous_node->nxt;
     }
+
+    // Set the node to be deleted
+    deleted_node = previous_node->nxt;
+
+    // The previous node'll point to the node after the position it'll be deleted
+    previous_node->nxt = previous_node->nxt->nxt;
+
+    // It no longer points to the node at next position
+    deleted_node->nxt = NULL;
 
     --size;
 
